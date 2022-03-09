b52034af611c7277e9a2aebf33b4e9699fbb1034
mah752 <83982869+mah752@users.noreply.github.com>
2022-03-09 20:16:21 +0000

    creating repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from "react";
+import "./App.css";
+import SearchEngine from "./SearchEngine";
+import lottie from "lottie-web";
 
 function App() {
+  const container = useRef(null);
+
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: container.current,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      animationData: require("./walk.json"),
+    });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <header>
+        <h1>What is the weather today?</h1>
+        <div className="container" ref={container}></div>
+        <SearchEngine />
+      </header>{" "}
+      <footer>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Coded by Maryan Hussein{" "}
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
